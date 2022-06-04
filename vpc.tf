05615ff9e07dbe5073101e9f493efcde4fac7aeb
azekura <azekura0106@gmail.com>
2022-06-04 21:10:23 +0900

    add vpce

--- vpc.tf
+++ vpc.tf
@@ -118,3 +118,94 @@ resource "aws_security_group" "allow_nfs" {
     Name = "minecraft-on-ecs"
   }
 }
+
+resource "aws_security_group" "vpc-endpoint" {
+  description = "VPCE Allowed Ports"
+  vpc_id      = aws_vpc.ecs-default.id
+
+  ingress = [
+    {
+      cidr_blocks = [
+        aws_vpc.ecs-default.cidr_block,
+      ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    },
+  ]
+
+  egress = [
+    {
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    },
+  ]
+
+  tags = {
+    "Description" = "VPCE Allowed Ports"
+    "Name"        = "VPCE default - VPCE SecurityGroup"
+  }
+}
+
+
+resource "aws_vpc_endpoint" "ecs_dkr" {
+  vpc_id              = aws_vpc.ecs-default.id
+  subnet_ids          = [aws_subnet.ecs-default.id]
+  security_group_ids  = [aws_security_group.vpc-endpoint.id]
+  service_name        = "com.amazonaws.ap-northeast-1.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ecs_api" {
+  vpc_id              = aws_vpc.ecs-default.id
+  subnet_ids          = [aws_subnet.ecs-default.id]
+  security_group_ids  = [aws_security_group.vpc-endpoint.id]
+  service_name      = "com.amazonaws.ap-northeast-1.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.ecs-default.id
+  service_name      = "com.amazonaws.ap-northeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  count           = length(aws_subnet.ecs-default)
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = aws_vpc.ecs-default.default_route_table_id
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = aws_vpc.ecs-default.id
+  subnet_ids          = [aws_subnet.ecs-default.id]
+  security_group_ids  = [aws_security_group.vpc-endpoint.id]
+  service_name        = "com.amazonaws.ap-northeast-1.logs"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.ecs-default.id
+  subnet_ids          = [aws_subnet.ecs-default.id]
+  security_group_ids  = [aws_security_group.vpc-endpoint.id]
+  service_name        = "com.amazonaws.ap-northeast-1.ssm"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+}
